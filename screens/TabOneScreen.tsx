b7351efabdda1f8bd910d1974820c6bac10ad651
brucetrindade <brucetrindade2000@gmail.com>
2022-09-08 09:16:18 -0300

    component text

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -6,10 +6,11 @@ import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+    <View style={{ backgroundColor: "blue"}}>
+      <Text style={styles.title}>Bem vindo ao primeiro app</Text>
+      {/* <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
   );
 }
